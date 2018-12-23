d85824a22d79f6b552d222652a66ccc7c85ef224
Babita Lugani <qababita@gmail.com>
2018-12-23 01:15:51 -0500

    added navigationPage

---
+++ src/main/java/com/qa/pages/navigationPage.java
@@ -0,0 +1,9 @@
+package com.qa.pages;
+
+public class navigationPage {
+
+	public void navigation() {
+		
+		System.out.println("navigation");
+	}
+}
---
+++ src/main/java/com/qa/pages/searcgPage.java
@@ -0,0 +1,10 @@
+package com.qa.pages;
+
+public class searcgPage {
+	
+	public void searchpage() {
+		
+		System.out.println("searchpage");
+	}
+
+}
